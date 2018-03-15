547e753590c4797194ee57f58c0ed26829a6fa77
“Ana” <amartinezaguilar6@gmail.com>
2018-03-15 19:43:58 +0100

    Created data structure

--- src/App.js
+++ src/App.js
@@ -5,69 +5,82 @@ class App extends Component {
      super(props);
      this.state = {
        listOfvehicles: [],
-       listOfDrivers: [],
      }
    }
 
    componentDidMount() {
 
-     //Drivers
-     fetch("http://localhost:3000/drivers")
-     .then(response => response.json())
-     .then(result => {
-       this.setState({
-         listOfDrivers: result,
-       })
-     })
-
-     //Vehicle
      fetch("http://localhost:3000/vehicles")
      .then(response => response.json())
-     .then(result => {
+     .then(resultVehicles => {
        this.setState({
-         listOfvehicles: result,
-       })
+         listOfvehicles: resultVehicles,
+       });
+       return fetch("http://localhost:3000/drivers");
      })
+     .then(response => response.json())
+     .then(resultDrivers => {
 
+       for (const vehicle of this.state.listOfvehicles) {
+         vehicle.drivers = []
+         for (const driver of resultDrivers) {
+           for (const vehicleOfDriver of driver.vehicles) {
+             if (vehicleOfDriver === vehicle.id) {
+               vehicle.drivers.push(driver);
+             }
+           }
+         }
+       }
+
+       this.setState({
+         listOfvehicles: this.state.listOfvehicles,
+       });
+
+     })
    }
+
 
    render() {
 
     return (
 
-      <ul>
+      <div>
 
-        {
-          this.state.listOfvehicles.map( (vehicle, index) => {
-            return (
-              <li key={index} className="cointainer">
-                <p> { vehicle.id } </p>
-                <p> { vehicle.type } </p>
 
-                {
-                  this.state.listOfDrivers.map( (driver, index) => {
-                    for (const vehicleOfDriver of driver.vehicles) {
-                      if (vehicleOfDriver === vehicle.id) {
-                        return (
-                          <div key={index} >
-                            <p> { driver.name } </p>
-                            <p> { driver.email } </p>
-                          </div>
-                        )
+
+        <ul>
+
+          {/* {
+            this.state.listOfvehicles.map( (vehicle, index) => {
+              return (
+                <li key={index} className="cointainer">
+                  <p> { vehicle.id } </p>
+                  <p> { vehicle.type } </p>
+
+                  {
+                    this.state.listOfDrivers.map( (driver, index) => {
+                      for (const vehicleOfDriver of driver.vehicles) {
+                        if (vehicleOfDriver === vehicle.id) {
+                          return (
+                            <div key={index} >
+                              <p> { driver.name } </p>
+                              <p> { driver.email } </p>
+                            </div>
+                          )
+                        }
                       }
-                    }
-                  })
-                }
+                    })
+                  }
 
-              </li>
-            )
-          })
-        }
+                </li>
+              )
+            })
+          } */}
 
-      </ul>
-
+        </ul>
 
 
+      </div>
     );
   }
 }
