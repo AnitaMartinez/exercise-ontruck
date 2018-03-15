e260a56ec9de0f47717a426bd97242591b9b9bda
“Ana” <amartinezaguilar6@gmail.com>
2018-03-15 14:43:52 +0100

    Linked vehicles with drivers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
+   constructor(props) {
+     super(props);
+     this.state = {
+       idVehicle: "",
+       listOfDrivers: [],
+     }
+   }
+
+   componentDidMount() {
+
+     //Drivers
+     fetch("http://localhost:3000/drivers")
+     .then(response => response.json())
+     .then(result => {
+       this.setState({
+         listOfDrivers: result,
+       })
+     })
+
+     //Vehicle
+     fetch("http://localhost:3000/vehicles")
+     .then(response => response.json())
+     .then(result => {
+       this.setState({
+         idVehicle: result[0].id,
+       })
+     })
+
+   }
+
+   render() {
+
+
+
     return (
-      <p>hola</p>
+
+      <div>
+
+        {
+          this.state.listOfDrivers.map( (driver, index) => {
+            for (const vehicle of driver.vehicles) {
+              if (vehicle === this.state.idVehicle) {
+                return (
+                  <p key={index} > {driver.name} </p>
+                )
+              }
+            }
+          })
+        }
+
+      </div>
+
+
+
     );
   }
 }
