5c0cabc0faad2f78019475f016bdacac23b2df66
“Ana” <amartinezaguilar6@gmail.com>
2018-03-18 12:32:20 +0100

    Added PropTypes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,41 +1,42 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
 class Cards extends Component {
 
-   render() {
+  render() {
 
-     const {listOfvehicles, valueInput} = this.props.state
+    const {listOfvehicles, valueInput} = this.props.state
 
-     const vehiclesByInputValue = listOfvehicles.filter( vehicle => {
-       return searchDriver(vehicle.drivers, valueInput);
-     })
-     function searchDriver(drivers, name){
-       for(let driver of drivers){
-         if (driver.name.toLowerCase().includes(name.toLowerCase())) {
-           return true;
-         }
-         if (driver.email.toLowerCase().includes(name.toLowerCase())) {
-           return true;
-         }
-       }
-       return false;
-     }
+    const vehiclesByInputValue = listOfvehicles.filter( vehicle => {
+      return searchDriver(vehicle.drivers, valueInput);
+    })
+    function searchDriver(drivers, name){
+      for(let driver of drivers){
+        if (driver.name.toLowerCase().includes(name.toLowerCase())) {
+          return true;
+        }
+        if (driver.email.toLowerCase().includes(name.toLowerCase())) {
+          return true;
+        }
+      }
+      return false;
+    }
 
     return (
-      <ul className="wrapper-grid">
-        {
-          vehiclesByInputValue.map( (vehicle, index) => {
-            return (
-              <li key={index} className="card">
+        <ul className="wrapper-grid">
+          {
+            vehiclesByInputValue.map( (vehicle, index) => {
+              return (
+                <li key={index} className="card">
 
-                <div> {(() => {
-                  switch (vehicle.type) {
-                    case "full_trailer": return <img src="icons/full-trailer.svg" alt="Full Trailer"/>;
-                    case "rigid_truck": return <img src="icons/rigid-truck.svg" alt="Rigid Truck"/>;
-                    case "box_van": return <img src="icons/box-van.svg" alt="Box Van"/>;
-                    case "van": return <img src="icons/van.svg" alt="Van"/>;
-                    default: ;
-                  }
+                  <div> {(() => {
+                    switch (vehicle.type) {
+                      case "full_trailer": return <img src="icons/full-trailer.svg" alt="Full Trailer"/>;
+                      case "rigid_truck": return <img src="icons/rigid-truck.svg" alt="Rigid Truck"/>;
+                      case "box_van": return <img src="icons/box-van.svg" alt="Box Van"/>;
+                      case "van": return <img src="icons/van.svg" alt="Van"/>;
+                      default: ;
+                    }
                   })()}
                 </div>
                 <p className="title-card"> { `${vehicle.drivers.length} Drivers` } </p>
@@ -51,7 +52,6 @@ class Cards extends Component {
                     })
                   }
                 </div>
-
               </li>
             )
           })
@@ -60,5 +60,9 @@ class Cards extends Component {
     );
   }
 }
+
+Cards.propTypes = {
+  state: PropTypes.object,
+};
 
 export default Cards;
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,19 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 
 const Input = ({ handleInput, valueInput }) => (
 
-    <div className="relative p-bottom-md">
-      <label className="hidden" htmlFor="inputByNameEmail">Busca por email o teléfono</label>
-      <input className="main-input" onChange={ handleInput } type="text" value={ valueInput } placeholder="Search ..." id="inputByNameEmail"/>
-      <img src="icons/search.svg" alt="Search" className="icon-search"/>
-    </div>
-  );
+  <div className="relative p-bottom-md">
+    <label className="hidden" htmlFor="inputByNameEmail">Busca por email o teléfono</label>
+    <input className="main-input" onChange={ handleInput } type="text" value={ valueInput } placeholder="Search ..." id="inputByNameEmail"/>
+    <img src="icons/search.svg" alt="Search" className="icon-search"/>
+  </div>
+);
+
+Input.propTypes = {
+  handleInput: PropTypes.func,
+  valueInput: PropTypes.string
+};
 
 export default Input;
